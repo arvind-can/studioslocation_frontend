71c9c387506f43d23cdb5f075e877216bc1eb1cb
Arvind CANDASSAMY <arvind.candassamy@gmail.com>
2026-02-11 14:01:13 +0100

    ToggleFilters added

--- src/components/search/filters/ToggleFilters.jsx
+++ src/components/search/filters/ToggleFilters.jsx
@@ -1,15 +1,25 @@
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group.jsx'
+import { useController } from 'react-hook-form'
 
-//As a part of a form, this component uses react hook form & yup and its register function for data passing and validation
-export default function CaracteristicsFilters({ name = 'Caractéristiques', caracteristics }) {
+//As a part of a form, this component uses react hook form.
+// This is a component controlled by useController.
+export default function ToggleFilters({ name, registerKey, control, filters }) {
+  const { field } = useController({ name: registerKey, control })
+
   return (
     <>
       <div className={'flex flex-col justify-between content-center gap-y-2'}>
         <p className={'text-sm pl-0.5'}>{name}</p>
-        <ToggleGroup className={'flex flex-row flex-wrap'} type='multiple' spacing={2}>
-          {caracteristics.map((item) => (
-            <ToggleGroupItem value={item.name} variant={'outline'}>
-              {item.logo} {item.name}
+        <ToggleGroup
+          className={'flex flex-row flex-wrap'}
+          type='multiple'
+          spacing={2}
+          value={field.value ?? []}
+          onValueChange={field.onChange}
+        >
+          {filters.map((filter) => (
+            <ToggleGroupItem key={filter.registerKey} value={filter.registerKey} variant={'outline'}>
+              {filter.symbol} {filter.name}
             </ToggleGroupItem>
           ))}
         </ToggleGroup>
