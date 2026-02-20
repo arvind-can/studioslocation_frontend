af3359c23155509beb0a372f2a6f824900555d7c
Arvind CANDASSAMY <arvind.candassamy@gmail.com>
2026-02-20 14:53:43 +0100

    Pagination on listings

---
+++ src/components/results/PaginationBar.jsx
@@ -0,0 +1,98 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from '@/components/ui/pagination'
+import { useNavigate } from 'react-router-dom'
+import { useSearchParams } from 'react-router'
+
+/**
+ * @param currentPageNo current page number (First page starting at 0)
+ * @param totalPageNo last page number (First page starting at 0)
+ * @returns {React.JSX.Element} a Pagination bar with previous next button and ellipsis handling for long pages
+ */
+export default function PaginationBar({ currentPageNo, totalPageNo }) {
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+
+  const changePage = (page) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('pageNo', page)
+    navigate(`/results?${params.toString()}`)
+  }
+
+  const delta = 2
+  let start = Math.max(currentPageNo - delta, 0)
+  let end = Math.min(currentPageNo + delta, totalPageNo - 1)
+
+  const paginationItems = []
+
+  // --- Première page + ellipsis début ---
+  if (start > 0) {
+    paginationItems.push(
+      <PaginationItem key={0}>
+        <PaginationLink isActive={currentPageNo === 0} onClick={() => changePage(0)}>
+          1
+        </PaginationLink>
+      </PaginationItem>
+    )
+
+    if (start > 1) {
+      paginationItems.push(
+        <PaginationItem key='start-ellipsis'>
+          <PaginationEllipsis />
+        </PaginationItem>
+      )
+    }
+  }
+
+  // --- Pages centrales ---
+  for (let cpt = start; cpt <= end; cpt++) {
+    paginationItems.push(
+      <PaginationItem key={cpt}>
+        <PaginationLink isActive={cpt === currentPageNo} onClick={() => changePage(cpt)}>
+          {cpt + 1}
+        </PaginationLink>
+      </PaginationItem>
+    )
+  }
+
+  // --- Dernière page + ellipsis fin ---
+  if (end < totalPageNo - 1) {
+    if (end < totalPageNo - 2) {
+      paginationItems.push(
+        <PaginationItem key='end-ellipsis'>
+          <PaginationEllipsis />
+        </PaginationItem>
+      )
+    }
+
+    paginationItems.push(
+      <PaginationItem key={totalPageNo - 1}>
+        <PaginationLink isActive={currentPageNo === totalPageNo - 1} onClick={() => changePage(totalPageNo - 1)}>
+          {totalPageNo}
+        </PaginationLink>
+      </PaginationItem>
+    )
+  }
+
+  return (
+    <Pagination className={'mt-5'}>
+      <PaginationContent>
+        <PaginationItem>
+          {currentPageNo > 0 && <PaginationPrevious onClick={() => changePage(currentPageNo - 1)} />}
+        </PaginationItem>
+
+        {paginationItems}
+
+        <PaginationItem>
+          {currentPageNo < totalPageNo - 1 && <PaginationNext onClick={() => changePage(currentPageNo + 1)} />}
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
+}
--- src/components/results/ResultsList.jsx
+++ src/components/results/ResultsList.jsx
@@ -4,29 +4,45 @@ import { useQuery } from '@tanstack/react-query'
 import ResultItem from '@/components/results/ResultItem.jsx'
 import { useSearchParams } from 'react-router'
 import { toast } from 'sonner'
+
 import { useEffect } from 'react'
+import PaginationBar from '@/components/results/PaginationBar.jsx'
 
 export default function ResultsList() {
   let [searchParams] = useSearchParams()
 
+  /**
+   * Query Function definition - Get listings with filters from url parameters
+   * @returns {Promise<*>} If query successful, return an object with data about listing results and pagination
+   */
   const studiosResults = async () => {
     const searchParamsObject = Object.fromEntries(searchParams)
     const studiosRep = await axios.get('http://localhost:8080/listings', { params: searchParamsObject })
-    return studiosRep.data.map((studio) => ({
+
+    //Build result object with relevant fields of found listings and pagination info
+    const resultObject = {}
+    resultObject.listings = studiosRep.data.results.map((studio) => ({
       id: studio.id,
       name: studio.title,
       rent: studio.rent,
       surface: studio.surface,
       image: 'https://images.unsplash.com/photo-1602146057681-08560aee8cde?q=80&w=640&auto=format&fit=crop'
     }))
+
+    resultObject.pagination = {
+      currentPage: studiosRep.data.pagination.page,
+      totalPages: studiosRep.data.pagination.totalPages
+    }
+    return resultObject
   }
 
+  //Send query on first page load
   const {
     data: studios,
     isError,
     error
   } = useQuery({
-    queryKey: ['listings'],
+    queryKey: ['listings', searchParams.toString()],
     queryFn: studiosResults,
     retry: false
   })
@@ -34,15 +50,25 @@ export default function ResultsList() {
   //Send the global toaster, the error signal
   useEffect(() => {
     if (isError) {
-      if (error.message) toast.error(error.message)
+      const errorMessage =
+        //Bonus : Send user validation message from the backend if he manipulated the params
+        error.status === 400
+          ? 'Erreur lors de la soumission du formulaire.\n Détails : ' + JSON.stringify(error.response.data)
+          : error.message
+      toast.error(errorMessage)
     }
   }, [isError, error])
 
   return (
-    <ItemGroup className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-      {studios?.map((item) => (
-        <ResultItem key={item.id} {...item} />
-      ))}
-    </ItemGroup>
+    <>
+      <ItemGroup className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {studios?.listings.map((item) => (
+          <ResultItem key={item.id} {...item} />
+        ))}
+      </ItemGroup>
+      {studios && (
+        <PaginationBar currentPageNo={studios.pagination.currentPage} totalPageNo={studios.pagination.totalPages} />
+      )}
+    </>
   )
 }
